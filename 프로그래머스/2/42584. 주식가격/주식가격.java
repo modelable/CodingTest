1def6491b7692ca5a6968c62a64b3f2f945921d3
modelable <62459471+modelable@users.noreply.github.com>
2024-07-15 14:09:32 +0900

    [level 2] Title: 주식가격, Time: 72.65 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,35 +2,22 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
+        int size = prices.length;
+        int[] answer = new int[size];
         Stack<int[]> stack = new Stack<>();
         stack.push(new int[]{0, prices[0]});
 
-        for (int i = 0; i < prices.length; i++) {
-            int size = prices.length;
-            int[] answer = new int[size];
-            Stack<int[]> stack = new Stack<>();
-            stack.push(new int[]{0, prices[0]});
-
-            for (int i = 1; i < size; i++) {
-                while (!stack.isEmpty() && (stack.peek()[1] > prices[i])) {
-                    int[] pop = stack.pop();
-                    answer[pop[0]] = i - pop[0];
-                }
-                stack.push(new int[]{i, prices[i]});
-            }
-
-            while (!stack.isEmpty()) {
+        for (int i = 1; i < size; i++) {
+            while (!stack.isEmpty() && (stack.peek()[1] > prices[i])) {
                 int[] pop = stack.pop();
-                answer[pop[0]] = size - pop[0] - 1;
+                answer[pop[0]] = i - pop[0];
             }
-
-            return answer;
+            stack.push(new int[]{i, prices[i]});
         }
 
         while (!stack.isEmpty()) {
             int[] pop = stack.pop();
-            answer[pop[0]] = prices.length - pop[0] - 1;
+            answer[pop[0]] = size - pop[0] - 1;
         }
 
         return answer;
