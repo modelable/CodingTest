72f698394f1d5ef48480f6b3640132c5a6031814
이기정 <kee@kee.local>
2024-07-15 14:08:40 +0900

    short coding(프로그래머스 42584)

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -7,19 +7,25 @@ class Solution {
         stack.push(new int[]{0, prices[0]});
 
         for (int i = 0; i < prices.length; i++) {
-            if (!stack.isEmpty()) {
-                int[] arr = stack.peek();
+            int size = prices.length;
+            int[] answer = new int[size];
+            Stack<int[]> stack = new Stack<>();
+            stack.push(new int[]{0, prices[0]});
 
-                if (arr[1] <= prices[i]) {
-                    stack.push(new int[]{i, prices[i]});
-                } else {
-                    while (!stack.isEmpty() && (stack.peek()[1] > prices[i])) {
-                        int[] pop = stack.pop();
-                        answer[pop[0]] = i - pop[0];
-                    }
-                    stack.push(new int[]{i, prices[i]});
+            for (int i = 1; i < size; i++) {
+                while (!stack.isEmpty() && (stack.peek()[1] > prices[i])) {
+                    int[] pop = stack.pop();
+                    answer[pop[0]] = i - pop[0];
                 }
+                stack.push(new int[]{i, prices[i]});
             }
+
+            while (!stack.isEmpty()) {
+                int[] pop = stack.pop();
+                answer[pop[0]] = size - pop[0] - 1;
+            }
+
+            return answer;
         }
 
         while (!stack.isEmpty()) {
