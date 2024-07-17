24167f6630b93902aacd9bd8f1489e3ddad03f60
modelable <62459471+modelable@users.noreply.github.com>
2024-07-17 22:07:12 +0900

    [level 3] Title: 표 편집, Time: 211.23 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -1,77 +1,82 @@
 import java.util.*;
-class Solution {
-    public String solution(int n, int k, String[] cmd) {
-        Stack<Col> stack = new Stack<>();
-        Col root = new Col(0);
-        Col prev = root;
-        Col iter = root;
-        for(int i=1; i<n; i++){
-            Col col = new Col(i);
-            prev.next = col;
-            col.prev = prev;
-            prev = col;
-            if(i==k) iter = col;
+
+public class Solution {
+    public static String solution(int n, int k, String[] cmd) {
+        Stack<Node> stack = new Stack<>(); // 휴지통
+        Node root = new Node(0);
+        Node prev = root;
+        Node pointer = null;
+
+        for(int i = 1; i < n; i++) {
+            Node node = new Node(i);
+
+            node.prev = prev;
+            node.prev.next = node;
+            prev = node;
+
+            if (i == k)
+                pointer = node;
         }
-        for(String query : cmd){
-            char key = query.charAt(0);
-            switch(key){
-                case 'U':
-                    for (int up = Integer.valueOf(query.substring(2)); up > 0; up--) {
-                        iter = iter.prev;
-                    }        
-                    break;
-                
-                case 'D':{
-                    int x = Integer.valueOf(query.substring(2));
-                    while((x--)>0){
-                        iter = iter.next;
+
+        for (String s : cmd) {
+            //System.out.println(s);
+            switch (s.charAt(0)) {
+                case 'U': // 현재 선택된 행에서 X칸 위에 있는 행을 선택합니다.
+                    for (int up = Integer.valueOf(s.substring(2)); up > 0; up--) {
+                        pointer = pointer.prev;
                     }
                     break;
-                }
-                case 'C':{
-                    stack.push(iter);
-                    iter = iter.delete();
+                case 'D': // 현재 선택된 행에서 X칸 아래에 있는 행을 선택합니다.
+                    for (int down = Integer.valueOf(s.substring(2)); down > 0; down--) {
+                        pointer = pointer.next;
+                    }
                     break;
-                }
-                case 'Z':{
-                    stack.pop().restore();
+                case 'C': // 현재 선택된 행을 삭제한 후,
+                    stack.push(pointer);
+
+                    if (pointer.prev != null) {
+                        pointer.prev.next = pointer.next;
+                    }
+
+                    if (pointer.next != null) {
+                        pointer.next.prev = pointer.prev;
+                        pointer = pointer.next;
+                    } else {
+                        pointer = pointer.prev;
+                    }
+
                     break;
-                }
+                case 'Z': // 가장 최근에 삭제된 행을 원래대로 복구합니다. 단, 현재 선택된 행은 바뀌지 않습니다.
+                    if (!stack.isEmpty()) {
+                        Node pop = stack.pop();
+
+                        if (pop.prev != null) pop.prev.next = pop;
+                        if (pop.next != null) pop.next.prev = pop;
+                    }
             }
+        }
+
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            answer.append('O');
+        }
+
+        while (!stack.isEmpty()) {
+            Node pop = stack.pop();
+            answer.setCharAt(pop.index, 'X'); // O(1)
         }
         
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < n; i++) {
-            sb.append('O');
-        }
-        while(!stack.isEmpty()) {
-            Col col = stack.pop();
-            sb.setCharAt(col.value,'X');
-        }
-        return sb.toString();
+        return answer.toString();
     }
-    class Col{
-        Col prev=null;
-        Col next=null;
-        int value;
-        Col(int index){
-            this.value = index;
-        }
-        void restore(){
-            if(this.prev!=null) this.prev.next = this;
-            if(this.next!=null) this.next.prev = this;
-        }
-        Col delete(){
-            Col next = this.next;
-            Col prev = this.prev;
-            if(prev!=null){
-                prev.next = this.next;
-            }
-            if(next!=null){
-                next.prev = this.prev;
-                return this.next;
-            }
-            return this.prev;
+
+    static class Node {
+        Node prev = null;
+        Node next = null;
+        int index;
+
+        Node(int index){
+            this.index = index;
         }
     }
 }
