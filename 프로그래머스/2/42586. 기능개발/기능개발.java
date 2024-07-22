a994044861d1afcbb0c37f7a13b3fbfe7d1928c3
modelable <62459471+modelable@users.noreply.github.com>
2024-07-22 14:07:36 +0900

    [level 2] Title: 기능개발, Time: 5.58 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        ArrayList<Integer> list = new ArrayList<>();
+        Queue<int[]> queue = new ArrayDeque<>();
+
+        // O(N): queue 초기화
+        for (int i = 0; i < progresses.length; i++) {
+            queue.add(new int[]{progresses[i], speeds[i]});
+        }
+
+        while (!queue.isEmpty()) {
+            if (queue.peek()[0] < 100) {
+                for (int i = 0; i < queue.size(); i++) {
+                    int[] poll = queue.poll();
+                    queue.add(new int[]{poll[0] + poll[1], poll[1]});
+                }
+            } else {
+                int count = 0;
+                while(!queue.isEmpty() && queue.peek()[0] >= 100) {
+                    queue.poll();
+                    count++;
+                }
+                list.add(count);
+            }
+        }
+
+        int[] answer = list.stream().mapToInt(Integer::intValue).toArray();
+        return answer;
+    }
+}
