8e1227a43788e7ef68142ac5cdf86a2275cf6f33
modelable <62459471+modelable@users.noreply.github.com>
2024-07-29 14:49:55 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 328.07 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,28 +2,16 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        HashMap<String, Integer> map = new HashMap<>();
-
-        for (String s : completion) {
-            if (!map.containsKey(s)) {
-                map.put(s, 1);
-            } else {
-                map.put(s, map.get(s) + 1);
+        
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        
+        for (int i = 0; i < completion.length; i++) {
+            if (!participant[i].equals(completion[i])) {
+                return participant[i];
             }
         }
 
-        for (String s : participant) {
-            if (map.containsKey(s)) {
-                if (map.get(s) == 1) {
-                    map.remove(s);
-                } else {
-                    map.replace(s, map.get(s) - 1);
-                }
-            } else {
-                return s;
-            }
-        }
-
-        return "error";
+        return participant[participant.length - 1];
     }
 }
