d85d6e1194b90508f97126a312359cf918be0d48
modelable <62459471+modelable@users.noreply.github.com>
2024-07-29 15:47:06 +0900

    [level 2] Title: 할인 행사, Time: 54.16 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -4,7 +4,7 @@ class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
         HashMap<String, Integer> map = new HashMap<>();
-
+        
         for (int i = 0; i < want.length; i++) {
             map.put(want[i], number[i]);
         }
@@ -13,23 +13,23 @@ class Solution {
         HashMap<String, Integer> map2 = new HashMap<>();
         for (int i = 0; i < 10; i++) {
             map2.put(discount[i], map2.getOrDefault(discount[i], 0) + 1);
+
+            if (map.equals(map2))
+                answer++;
         }
 
         // 비교
-        for (int i = 0; i < discount.length - 9; i++) {
-            for (int j = 0; j < want.length; j++) {
-                if (!(map.get(want[j]).equals(map2.getOrDefault(want[j], 0))))
-                    break;
-                
-                if (j == want.length - 1) {
-                    answer++;
-                }
+        for (int i = 0; i < discount.length - 10; i++) {
+
+            map2.put(discount[i + 10], map2.getOrDefault(discount[i + 10], 0) + 1);
+            if (map2.get(discount[i]) == 1) {
+                map2.remove(discount[i]);
+            } else {
+                map2.put(discount[i], map2.get(discount[i]) - 1);
             }
 
-            if (i < discount.length - 10) {
-                map2.put(discount[i + 10], map2.getOrDefault(discount[i + 10], 0) + 1);
-                map2.replace(discount[i], map2.get(discount[i]) - 1);
-            }
+            if (map.equals(map2))
+                answer++;
         }
 
         return answer;
