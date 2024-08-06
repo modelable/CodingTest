2de06e6d9e2ae97b42ac0dbc9bbfd79e5022d198
modelable <62459471+modelable@users.noreply.github.com>
2024-08-06 20:00:31 +0900

    [level 3] Title: 양과 늑대, Time: 6.23 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,17 +1,20 @@
 import java.util.*;
+
 class Solution {
+
     private static int result;
+
     public static int solution(int[] info, int[][] edges) {
-        int answer = 0;
         Node[] nodes = new Node[info.length];
 
+        // 노드 초기화
         for (int i = 0; i < info.length; i++) {
             Node node = new Node();
             node.animal = info[i];
-            node.index = i;
             nodes[i] = node;
         }
 
+        // 트리 생성
         for (int i = 0; i < edges.length; i++) {
             if (nodes[edges[i][0]].leftChild == null) {
                 nodes[edges[i][0]].leftChild = nodes[edges[i][1]];
@@ -20,21 +23,16 @@ class Solution {
             }
         }
 
+        // 시뮬레이션
         ArrayList<Node> list = new ArrayList<>();
         list.add(nodes[0]);
         simulate(0, 0, list, nodes[0]);
-        System.out.println("result: " + result);
+
         return result;
     }
 
     public static void simulate(int sheep, int wolf, ArrayList<Node> list, Node node) {
         if (list.isEmpty()) return;
-//        //==debug==//
-//        System.out.print("now: " + node.index + " ");
-//        for (Node x : list) {
-//            System.out.print(x.index);
-//        }
-//        System.out.println();
 
         list.remove(node);
         if (node.leftChild != null) list.add(node.leftChild);
@@ -44,15 +42,9 @@ class Solution {
         else if (node.animal == 1) wolf++;
 
         if (sheep <= wolf) return;
-        if (result < sheep) {
-            result = sheep;
-            System.out.print("!!now: " + node.index + " ");
-            for (Node x : list) {
-                System.out.print(x.index);
-            }
-            System.out.println("sheep:" + sheep);
 
-        }
+        // 최댓값 갱신
+        if (result < sheep) result = sheep;
 
         for (Node n : list) {
             simulate(sheep, wolf, new ArrayList<>(list), n);
@@ -61,7 +53,6 @@ class Solution {
 
     static class Node {
         int animal; //0: 양, 1: 늑대
-        int index;
         Node leftChild;
         Node rightChild;
     }
