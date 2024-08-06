24b07ba7b1587e8daa4cee3b97f238250b96971e
modelable <62459471+modelable@users.noreply.github.com>
2024-08-06 16:47:18 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 91.61 ms, Memory: 138 MB -BaekjoonHub

---
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        int[] answer = new int[enroll.length];
+        HashMap<String, Integer> map = new HashMap<>();
+        Node[] nodes = new Node[enroll.length];
+
+        // 트리 초기화(O(N))
+        for (int i = 0; i < enroll.length; i++) {
+            map.put(enroll[i], i);
+            Node node = new Node();
+            node.name = enroll[i];
+            if (!referral[i].equals("-"))
+                node.parent = nodes[map.get(referral[i])];
+            nodes[i] = node;
+        }
+
+        // 이익금 계산(O(M*N))
+        for (int i = 0; i < seller.length; i++) {
+            String sellerName = seller[i];
+            int profit = amount[i] * 100;
+            answer[map.get(sellerName)] += profit - (int) (profit * 0.1);
+
+            while (nodes[map.get(sellerName)].parent != null && profit > 0) {
+                sellerName = nodes[map.get(sellerName)].parent.name;
+                profit = (int) (profit * 0.1);
+                answer[map.get(sellerName)] += profit - (int) (profit * 0.1);
+            }
+        }
+
+        return answer;
+    }
+
+    static class Node {
+        String name = null;
+        Node parent = null;
+    }
+}
