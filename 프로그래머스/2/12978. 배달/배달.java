573ecc2a65b072be541241749a9207b054f99507
modelable <62459471+modelable@users.noreply.github.com>
2024-08-22 17:32:00 +0900

    [level 2] Title: 배달, Time: 7.92 ms, Memory: 85.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -0,0 +1,80 @@
+import java.util.*;
+
+class Solution {
+
+    public static int solution(int N, int[][] road, int K) {
+        //정렬
+        Arrays.sort(road, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0] && o1[1] == o2[1]) {
+                    return o1[2] - o2[2];
+                } else if (o1[0] == o2[0]) {
+                    return o1[1] - o2[1];
+                } else
+                    return o1[0] - o2[0];
+            }
+        });
+        
+        //인접 리스트 초기화
+        ArrayList<Node>[] adjList = new ArrayList[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < road.length; i++) {
+            int u = road[i][0];
+            int v = road[i][1];
+            int w = road[i][2];
+
+            adjList[u].add(new Node(v, w));
+            adjList[v].add(new Node(u, w));
+        }
+
+        //방문 배열
+        boolean[] visited = new boolean[N + 1];
+        
+        //최소 거리
+        int[] dist = new int[N + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[1] = 0;
+            
+        //우선순위 큐
+        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> o1.cost - o2.cost);
+        pq.add(new Node(1, 0));
+
+        while (!pq.isEmpty()) {
+            Node node = pq.poll();
+
+            if (visited[node.dest]) continue;
+            visited[node.dest] = true;
+
+            for (Node next : adjList[node.dest]) {
+                if (node.cost + next.cost < dist[next.dest]) {
+                    dist[next.dest] = node.cost + next.cost;
+                    pq.add(new Node(next.dest, dist[next.dest]));
+                }
+            }
+        }
+        
+        int answer = 0;
+
+        for (int i = 1; i <= N; i++) {
+            if (dist[i] <= K)
+                answer++;
+        }
+
+        return answer;
+    }
+
+    public static class Node {
+        private int dest;
+        private int cost;
+
+        public Node(int dest, int cost) {
+            this.dest = dest;
+            this.cost = cost;
+        }
+    }
+}
