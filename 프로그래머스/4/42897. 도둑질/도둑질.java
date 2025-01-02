8329164bb78037ae11e448998a85478cba00feba
modelable <62459471+modelable@users.noreply.github.com>
2025-01-02 22:25:30 +0900

    [level 4] Title: 도둑질, Time: 23.68 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -1,7 +1,4 @@
 public class Solution {
-    
-    public static void main(String[] args) {
-    }
 
     public static int solution(int[] money) {
         int a = calc(0, money);
@@ -10,48 +7,19 @@ public class Solution {
         return Math.max(a, b);
     }
 
-    public static int calc(int i, int[] money) {
+    public static int calc(int n, int[] money) {
         int N = money.length;
         int[] dp = new int[N]; //money
-        boolean firstElementUsed1 = false;
-        boolean firstElementUsed2 = false;
 
-        dp[i] = money[i];
-        firstElementUsed1 = true;
+        dp[n] = money[n];
 
-        if (money[i] > money[i + 1]) {
-            dp[i + 1] = money[i];
-            firstElementUsed2 = true;
-        } else {
-            dp[i + 1] = money[i + 1];
+        if (money[n] > money[n + 1]) dp[n + 1] = money[n];
+        else dp[n + 1] = money[n + 1];
+
+        for (int i = n + 2; i < N - 1 + n; i++) {
+            dp[i] = Math.max(dp[i - 2] + money[i], dp[i - 1]);
         }
 
-        if (i == 1) {
-            firstElementUsed1 = false;
-            firstElementUsed2 = false;
-        }
-
-        for (i = i + 2; i < N - 1; i++) {
-            if (dp[i - 2] + money[i] > dp[i - 1]) {
-                dp[i] = dp[i - 2] + money[i];
-                boolean temp = firstElementUsed1;
-                firstElementUsed1 = firstElementUsed2;
-                firstElementUsed2 = temp;
-            } else
-                dp[i] = dp[i - 1];
-        }
-
-        //마지막 원소
-        if (firstElementUsed1) {
-            int max = Math.max(Math.max(dp[i - 1], dp[i - 2]), dp[i - 2] - money[0] + money[i]);
-            dp[i] = max;
-        } else {
-            if (dp[i - 2] + money[i] > dp[i - 1]) {
-                dp[i] = dp[i - 2] + money[i];
-            } else
-                dp[i] = dp[i - 1];
-        }
-
-        return dp[i];
+        return dp[N - 2 + n];
     }
 }
